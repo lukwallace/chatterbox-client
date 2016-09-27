ba7b27cc7d68962cb290622d4a1f5ff5a07be1d6
lukwallace <wallace.s.luk@gmail.com>
2016-09-27 12:35:46 -0700

    Implement formating for friends' message, changing room, and posting messages.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,12 +1,13 @@
 // YOUR CODE HERE:
 var filter = {};
+var defaultRoom = 'lobby';
 var app = {};
 app.friendList = {};
 app.server = 'https://api.parse.com/1/classes/messages';
-app.user = window.location.href.slice(68);
-app.roomname = 'lobby';
+app.user = window.location.href.slice(window.location.href.indexOf('username=') + 9);
+app.roomList = {};
 
-app.send = function(message) {
+app.send = function(message, callback) {
   $.ajax({
   // This is the url you should use to communicate with the parse API server.
     url: app.server,
@@ -15,6 +16,7 @@ app.send = function(message) {
     contentType: 'application/json',
     success: function (data) {
       console.log('chatterbox: Message sent', data);
+      callback();
     },
     error: function (data) {
       // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -44,16 +46,24 @@ app.clearMessages = function() {
   $('#chats').empty();
 };
 
-app.renderMessage = function(message) {
-  var $userName = $('<a class="username" >' + message.username + '</a>' );
-  $userName.click(app.handleUsernameClick.bind(null, message.username));
+app.renderMessage = function(message, mode) {
+  var $userName = $('<a class="username" ></a>' );
+  $userName.text(message.username);
+  $userName.click(app.handleUsernameClick.bind(null, $userName.text()));
   var $message = $('<div></div>');
+  $message.addClass('chat');
   var $text = $('<h3></h3>').text(message.text);
-  var $time = $('<h3>' + message.updatedAt + '</h3>');
+  var $time = $('<h3></h3>').text(message.updatedAt);
+  var $room = $('<h3></h3>').text(message.roomname);
   $message.append($userName);
   $message.append($text);
   $message.append($time);
-  $('#chats').append($message);
+  $message.append($room);
+  if ( mode ) {
+    $('#chats').prepend($message);
+  } else {
+    $('#chats').append($message);
+  }
 };
 
 app.init = function () {
@@ -65,21 +75,45 @@ app.init = function () {
     $textbox.val('');
   });
   app.fetch( function(obj) {
+    app.renderRoom(defaultRoom);
     var messages = obj.results;
     for (var i = 0; i < messages.length; i++) {
-      app.renderMessage(messages[i]);
+      if (messages[i].roomname === defaultRoom) {
+        app.renderMessage(messages[i]);
+      }
       app.renderRoom(messages[i].roomname);
     }
   });
-  // app.renderRoom(app.roomname);
+};
+
+app.changeRoom = function(value) {
+  app.clearMessages();
+  filter = {roomname: value};
+  app.fetch(function(obj) {
+    for (var i = 0; i < obj.results.length; i++) {
+      app.renderMessage(obj.results[i]);
+    }
+  });
 };
 
 app.renderRoom = function(roomName) {
-  var $newRoom = $('<option value=\"' + roomName + '\">' + roomName + '</option>');
-  $('#roomSelect').append($newRoom);
+  if (!(roomName in app.roomList)) {
+    var $newRoom = $('<option></option>');
+    var $pocket = $('<span></span>').text(roomName);
+    var escaped = $pocket.text(roomName).html();
+    $newRoom.attr('value', escaped);
+    $newRoom.text(roomName);
+    $('#roomSelect').append($newRoom);
+    app.roomList[roomName] = 1;
+  }
 };
 
 app.handleUsernameClick = function(username) {
+  $('.username').each(function () {
+    if ($(this).text() === username) {
+      $(this).toggleClass('friend');
+    }
+  });
   app.friendList[username] = 1;
 };
 
@@ -87,9 +121,20 @@ app.handleSubmit = function(text) {
   var message = {
     username: app.user,
     text: text,
-    roomname: app.roomname
+    roomname: filter.roomname || defaultRoom
   };
-  app.send(message);
+  app.send(message, app.refresh);
+};
+
+app.refresh = function() {
+  var currentTime = $('#chats :first :nth-child(3)').text();
+  var params = {updatedAt: {$gt: currentTime}};
+  app.fetch( function(obj) {
+    var messages = obj.results;
+    for (var i = 0; i < messages.length; i++) {
+      app.renderMessage(messages[i], true);
+    }
+  }, params);
 };
 
 $(document).ready(app.init);
